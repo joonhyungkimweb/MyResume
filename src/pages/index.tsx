5b0fbb087848343ea5a28b09a7bec753bdee31d3
joonhyung <asy8010@gmail.com>
2024-04-25 10:03:29 +0900

    [feat] 프로필 헤더 반응형 처리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,45 +5,50 @@ import LinkedIn from "../images/icons/linkedin.svg";
 import Github from "../images/icons/github.svg";
 import Email from "../images/icons/email.svg";
 
-// email : asy8010@gmail.com
-// linkedIn :
-// github :
+const profileLinks = [
+  {
+    link: "mailto:asy8010@gmail.com",
+    icon: Email,
+    alt: "send email",
+  },
+  {
+    link: "https://www.linkedin.com/in/joon-hyung-kim-682563252",
+    icon: LinkedIn,
+    alt: "link to linkedin",
+  },
+  {
+    link: "https://github.com/joonhyungkimweb",
+    icon: Github,
+    alt: "link to github",
+  },
+];
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main className="py-24 flex justify-center">
-      <header className="flex flex-wrap max-w-[700px] w-full justify-between">
-        <figure className="w-72 mr-10">
-          <img src={Profile} className="rounded-full w-full y-full" />
-        </figure>
-        <section className="flex flex-col justify-center items-end">
-          <h1 className="text-5xl">김준형</h1>
+      <header className="flex flex-wrap max-w-[768px] w-full gap-10 justify-center">
+        <section className="flex flex-col justify-center items-center md:items-end order-2 md:order-1">
+          <h1 className="text-7xl">김준형</h1>
           <h2 className="text-gray-500">Fullstack Web engineer</h2>
-          <ul className="mt-3 flex">
-            <li className="mr-2">
-              <a href="mailto:asy8010@gmail.com">
-                <img src={Email} className="w-5 h-5" alt="send email" />
-              </a>
-            </li>
-            <li className="mr-2">
-              <a
-                href="https://www.linkedin.com/in/joon-hyung-kim-682563252"
-                target="_blank"
-              >
-                <img
-                  src={LinkedIn}
-                  className="w-5 h-5"
-                  alt="link to linkedin"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/joonhyungkimweb" target="_blank">
-                <img src={Github} className="w-5 h-5" alt="link to github" />
-              </a>
-            </li>
+          <ul className="mt-3 flex gap-3">
+            {profileLinks.map(({ alt, icon, link }) => (
+              <li>
+                <a
+                  href={link}
+                  target={link.split(":")[0] === "mailto" ? "_self" : "_blank"}
+                >
+                  <img src={icon} className="w-5 aspect-square" alt={alt} />
+                </a>
+              </li>
+            ))}
           </ul>
         </section>
+        <figure className="w-full flex justify-center order-1 md:w-auto md:order-2">
+          <img
+            src={Profile}
+            className="rounded-full max-w-[420px] sm:max-w-[300px] aspect-square"
+          />
+        </figure>
       </header>
     </main>
   );
