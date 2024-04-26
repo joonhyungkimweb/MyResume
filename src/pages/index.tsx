a35b3c4b47a16c1fb797f7653878116e1cb6d1cc
joonhyung <asy8010@gmail.com>
2024-04-27 02:42:27 +0900

    [feat] 기술 서술 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,12 +105,36 @@ const projects = [
 ];
 
 const skills = {
-  Language: ["TS/JS", "Python", "Swift"],
-  "FE/App": ["React", "React-native", "Ios"],
-  BE: ["Express", "Nest.js", "FastAPI"],
-  DB: ["MongoDB", "Postgresql", "Mysql"],
-  Cloud: ["AWS", "GCP"],
-  DevOps: ["Docker", "Kubernetes"],
+  Programming: [
+    "사용하는 언어의 Idiom과 탐구하고 convention을 준수하는 것을 즐깁니다.",
+    "함수형과 객체 지향 패러다임을 이해하고 적재적소에 사용합니다.",
+    "Javascript/Typescript에 능숙합니다. Typescript의 타입 조작 키워드를 사용하여 TS가 타입 추론을 잘 할 수 있게 격려합니다.",
+    "Python의 방대한 AI 관련 생태계를 활용하고 환경을 구성하는데에 익숙합니다.",
+  ],
+  Frontend: [
+    "주로 React를 사용해 Frontend를 개발합니다. 함수형 컴포넌트와 Hooks를 능숙하게 사용하고 로직을 적절히 분리합니다.",
+    "날 것 그대로의 React를 탐구하는 한 편 생산성을 높이는 React 생태계에도 큰 관심을 기울입니다.",
+    "컴포넌트의 디자인 패턴에 대해 숙고하고 우아한 컴포넌트 분리를 위해 노력합니다.",
+    "시맨틱한 마크업과 웹 접근성 향상을 지향합니다.",
+  ],
+  Backend: [
+    "Express, Nest.js, fastAPI 등의 프레임워크를 사용해 웹서버를 구축합니다.",
+    "ORM을 사용하여 영속 데이터를 코드 중심으로 처리하는 것을 선호합니다.",
+    "Testing Library로 테스트 코드를 작성하고 테스트 주도 개발을 실천하려 노력합니다.",
+    "기능을 구현하는 것 뿐만이 아닌, 보다 효율적인 서비스 구조를 구축하기 위해 고민합니다.",
+  ],
+  Devops: [
+    "Docker image 경량화 및 최적화를 위해 노력합니다.",
+    "Kubernetes를 사용해 제한된 리소스를 보다 효율적으로 관리해 본 경험이 있습니다.",
+    "클라우드 환경에서 제품을 배포하는데에 익숙하며, 다양한 클라우드 공급자 환경에서 CI/CD를 구축할 수 있습니다.",
+    "온프레미스와 클라우드 간, 혹은 서로 다른 클라우드 공급자 사이의 리소스를 이전하거나 각기 다른 환경을 통합해 아키텍처를 구축한 경험이 있습니다.",
+  ],
+  Others: [
+    "Git을 사용한 버전 관리에 익숙하며 한 커밋 한 커밋이 일관된 작업 내용을 담도록 하는 데에 많은 노력을 기울입니다. patch 옵션을 애용하고, 필요하다면 공격적인 rebase로 히스토리를 바로잡습니다.",
+    "Jira, Notion 등 협업용 도구 사용에 익숙합니다.",
+    "문제 해결을 위해 가능한 모든 방법을 탐구하고 필요하다면 낯선 분야라도 적극적으로 탐색합니다.",
+    "더 많은 기술을 익히기 위해 몰두하는 것을 지양합니다. 익힌 기술로 현실의 문제에 도전하고 이를 해결했을 때 성장을 체감합니다.",
+  ],
 };
 
 const BlueDotttedText = ({ children }: React.PropsWithChildren) => (
@@ -210,8 +234,8 @@ const IndexPage: React.FC<PageProps> = () => {
               details,
               stacks,
             }) => (
-              <li className="flex flex-wrap">
-                <div className="basis-full mb-5 md:basis-72 md:mb-10 md:mr-8 md:text-end">
+              <li className="flex flex-wrap mb-12 md:mb-20">
+                <div className="basis-full mb-5 md:basis-72 md:mb-0 md:mr-8 md:text-end">
                   <h3 className="text-3xl font-medium mb-2 md:text-4xl">
                     {link && <span className="text-indigo-500">#</span>}
                     <a href={link} target="_blank">
@@ -250,7 +274,7 @@ const IndexPage: React.FC<PageProps> = () => {
                     </div>
                   )}
                 </div>
-                <div className="relative basis-full md:basis-[calc(100%-20rem)] pl-4 border-l-2 border-l-indigo-500 mb-12">
+                <div className="relative basis-full md:basis-[calc(100%-20rem)] pl-4 border-l-2 border-l-indigo-500">
                   <p className="whitespace-pre-line mb-4 md:text-lg">
                     {details}
                   </p>
@@ -265,16 +289,16 @@ const IndexPage: React.FC<PageProps> = () => {
         <h2 className="text-5xl md:text-6xl mb-12 md:mb-16 font-medium">
           <BlueDotttedText>기술</BlueDotttedText>
         </h2>
-        <ul className="flex flex-wrap">
+        <ul>
           {Object.entries(skills).map(([category, skills]) => (
             <li
-              className="p-3 basis-1/2 sm:basis-1/3 md:basis-1/4"
+              className="py-12 px-3 border-b last:border-b-0 last:pb-0 first:pt-0 md:py-16"
               key={category}
             >
-              <h3 className="text-2xl md:text-3xl font-medium mb-2">
+              <h3 className="text-3xl font-medium mb-2 md:text-4xl">
                 {category}
               </h3>
-              <ul className="text-xl">
+              <ul className="md:text-lg">
                 {skills.map((skill) => (
                   <StyledListItem key={skill}>{skill}</StyledListItem>
                 ))}
